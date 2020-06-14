b831461ef9ed1f8c43ce33d485608bf78a1fabeb
Andrei Abramchuk <stilegs@gmail.com>
2020-06-14 17:48:18 +0300

    feat: add empty header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.scss';
+import Header from './components/header/header';
 
 function App() {
   return (
     <div>
-      <h1>Hello RsLang!</h1>
+      <Header />
+      <h2>Dashboard</h2>
     </div>
   );
 }
---
+++ src/components/header/header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './header.scss';
+
+const Header = () => (
+  <div className="header">
+    <h1>RSLang Header</h1>
+  </div>
+);
+
+export default Header;
