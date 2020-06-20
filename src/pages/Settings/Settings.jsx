48d401ebe38d58d497f3883843631c0f09a76a63
Andrey Lavrov <fanton96mailru@gmail.com>
2020-06-20 16:52:35 +0300

    feat: add settings page

---
+++ src/pages/Settings/Settings.jsx
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react';
+import './Settings.scss';
+
+export default class WordConstructor extends PureComponent {
+  render() {
+    return (
+      <div>
+        <h1>Settings</h1>
+      </div>
+    );
+  }
+}
