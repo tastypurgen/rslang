8deafeddd3438183883ac3c134cc495a451a346c
Alina Sidorova <alinasidorova2812@gmail.com>
2020-06-23 11:19:43 +0300

    feat: replace info links to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import './Footer.scss';
 
 const Header = () => (
   <div className="footer">
+    <div className="info">
+      <NavLink className="promo" to="/promo-page">
+        Промо
+      </NavLink>
+      <NavLink className="about" to="/about-team">
+        О команде
+      </NavLink>
+    </div>
     <span>Copyright &#169; 2020 Все права защищены</span>
   </div>
 );
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 
 import './Dashboard.scss';
 
@@ -8,14 +7,6 @@ class Dashboard extends React.PureComponent {
     return (
       <div className="dashboard">
         <h2>Dashboard</h2>
-        <div className="info">
-          <NavLink className="promo" to="/promo-page">
-            Промо
-          </NavLink>
-          <NavLink className="about" to="/about-team">
-            О команде
-          </NavLink>
-        </div>
       </div>
     );
   }
