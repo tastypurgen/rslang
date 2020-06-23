a8a2ab901b857f9929884d513be19a51b5b23301
Andrey Lavrov <fanton96mailru@gmail.com>
2020-06-23 23:34:25 +0300

    feat: add saveSettings func into Settings service

--- src/services/settingsService.js
+++ src/services/settingsService.js
@@ -1,6 +1,4 @@
-const token = localStorage.getItem('userToken');
-const userId = localStorage.getItem('userId');
-const getUserSettings = async () => {
+const getUserSettings = async (token, userId) => {
   let rawResponse = null;
   try {
     rawResponse = await fetch(`https://afternoon-falls-25894.herokuapp.com/users/${userId}/settings`, {
@@ -27,4 +25,82 @@ const getUserSettings = async () => {
   return response;
 };
 
-export default getUserSettings;
+const setUserSettings = async (token, userId, settingsObj) => {
+  const jsonSettings = JSON.stringify(settingsObj);
+
+  let rawResponse = null;
+  try {
+    rawResponse = await fetch(`https://afternoon-falls-25894.herokuapp.com/users/${userId}/settings`, {
+      method: 'PUT',
+      withCredentials: true,
+      headers: {
+        Authorization: `Bearer ${token}`,
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: jsonSettings,
+    });
+  } catch (err) {
+    return err;
+  }
+
+  if (rawResponse.status !== 200) {
+    return {
+      status: rawResponse.status,
+      text: rawResponse.statusText,
+    };
+  }
+  const response = await rawResponse.json();
+  response.status = rawResponse.status;
+  return response;
+};
+
+const setDefaultSettings = async (token, userId) => {
+  const defaultSettings = {
+    wordsPerDay: 20,
+    optional: {
+      associationImage: true,
+      autoPronunciation: false,
+      displayAssessmentBtns: true,
+      displayDeleteBtn: true,
+      displayDifficultBtn: true,
+      displayShowAnswerBtn: true,
+      exampleSentence: true,
+      explanationSentence: true,
+      isRequiredInputChecked: true,
+      maxCardsPerDay: 20,
+      showWordAndSentenceTranslation: true,
+      wordTranscription: true,
+      wordTranslation: true,
+    },
+  };
+  const jsonSettings = JSON.stringify(defaultSettings);
+
+  let rawResponse = null;
+  try {
+    rawResponse = await fetch(`https://afternoon-falls-25894.herokuapp.com/users/${userId}/settings`, {
+      method: 'PUT',
+      withCredentials: true,
+      headers: {
+        Authorization: `Bearer ${token}`,
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: jsonSettings,
+    });
+  } catch (err) {
+    return err;
+  }
+
+  if (rawResponse.status !== 200) {
+    return {
+      status: rawResponse.status,
+      text: rawResponse.statusText,
+    };
+  }
+  const response = await rawResponse.json();
+  response.status = rawResponse.status;
+  return response;
+};
+
+export { getUserSettings, setUserSettings, setDefaultSettings };
