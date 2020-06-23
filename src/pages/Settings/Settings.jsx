d5e9636f78c22ce67d5b003dcb759a6a1b431d19
Alina Sidorova <alinasidorova2812@gmail.com>
2020-06-24 01:46:06 +0300

    feat: add header

---
+++ src/pages/Settings/Settings.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Settings.scss';
+
+const Settings = () => (
+  <div className="settings">
+    <h2>Settings</h2>
+  </div>
+);
+
+export default Settings;
---
+++ src/pages/Statistic/Statistic.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Statistic.scss';
+
+const Statistic = () => (
+  <div className="statistic">
+    <h2>Statistic</h2>
+  </div>
+);
+
+export default Statistic;
