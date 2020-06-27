5770a0a241f7349a010c3c18d30bd03e4bcd69ef
NikitaKolpinskiy <nikitakolpinski@mail.ru>
2020-06-27 16:49:23 +0300

    Add: main logic, basic html and css

--- src/pages/Sprint/Sprint.jsx
+++ src/pages/Sprint/Sprint.jsx
@@ -1,12 +1,154 @@
-import React, { PureComponent } from 'react';
 import './Sprint.scss';
+import React, {useEffect, useState} from 'react';
+import RenderTime from './timer';
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+import speaker from './image/speaker.png';
 
-export default class Sprint extends PureComponent {
-  render() {
+
+function Sprint () {
+    const source = 'https://raw.githubusercontent.com/tastypurgen/rslang-data/master/';
+
+    let [pageNumber, setPageNumber] = useState(0);
+    let time = 60;
+    let [winStreak, setWinStreak] = useState(0);
+    let [word, setWord] = useState({
+        enWord: '', 
+        ruWord:'',
+        correct: true
+    });
+    
+    let [wordsPool, setWordsPool] = useState();
+    let [score, setScore] =  useState(0); 
+    let [soundSrc, setSoundSrc] = useState({
+        audioSrc: ''
+    })
+
+   
+    function loadWord() {
+        const url = `https://afternoon-falls-25894.herokuapp.com/words?page=${pageNumber}&group=0`;
+        let wordsData = JSON.parse(localStorage.getItem("wordsPool"));
+        if (wordsData) {
+            setWordsPool(wordsData);
+            return Promise.resolve(wordsData); 
+        }
+
+        return fetch(url).then(resp => resp.json()).then(wordsList => {
+            setWordsPool(wordsList);
+            localStorage.setItem("wordsPool", JSON.stringify(wordsList));
+            return Promise.resolve(wordsList);
+        });
+    }
+
+    function startGame(wordsList) {
+        const useCorrectWord =  Math.random() >= 0.5;
+        const lastWord = wordsList.pop();
+        const randomIndex = Math.floor(Math.random() * wordsList.length - 1);
+        setSoundSrc({
+            audioSrc: source + lastWord.audio
+        })
+
+        useCorrectWord ? 
+            setWord({
+                enWord  : lastWord.word,
+                ruWord  : lastWord.wordTranslate,
+                correct : true
+            })
+            :
+            setWord({
+                enWord  : lastWord.word,
+                ruWord  : wordsList[randomIndex].wordTranslate,
+                correct : false
+            })
+
+    }
+
+    function playSound() {
+        const sound = new Audio();
+  
+        sound.src = soundSrc.audioSrc;
+        sound.play();
+    }
+
+    function updateScore(isAnswerCorrect) {
+        if (isAnswerCorrect) {
+            setWinStreak(++winStreak)
+            winStreak >= 4 ? setScore(score + 20) : setScore(score + 10);
+        } else {
+            setWinStreak(0);
+            setScore(score);
+        }
+    }
+    
+    function arrowEvent(event) {
+        if (event.key === 'ArrowLeft') {
+            updateScore(!word.correct)
+            startGame(wordsPool)
+        } else if(event.key === 'ArrowRight') {
+            updateScore(word.correct)
+            startGame(wordsPool)
+        }
+    }
+
+    useEffect(() => {
+        loadWord().then(words => startGame(words));
+        
+    }, [])
+  
     return (
-      <div>
-        <h1>Sprint</h1>
-      </div>
-    );
-  }
+        <section  onKeyDown={arrowEvent}  className='mainSection' tabIndex="0">
+            <div className='wrapper'>
+                <div>{score}</div>
+                <div className='gameContainer'>
+                    <div key='1'>{word.enWord}</div>
+                    <div>{word.ruWord}</div>
+                    <div>
+                        <button onClick={() =>  {
+                            updateScore(!word.correct);
+                            if (!wordsPool.length) {
+                                localStorage.removeItem("wordsPool")
+                                setPageNumber(++pageNumber);
+                                debugger
+                                loadWord().then(words => startGame(words));
+                            } else {
+                                startGame(wordsPool)
+                            }
+                            }}>Неверно</button>
+                        <button onClick={() =>  {
+                            updateScore(word.correct);
+                            if (!wordsPool.length) {
+                                localStorage.removeItem("wordsPool")
+                                setPageNumber(++pageNumber);
+                                loadWord().then(words => startGame(words));
+                            } else {
+                                startGame(wordsPool);
+                            }
+                            }}>Верно</button>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <button>&#8592;</button>
+                        <button>&#8594;</button>
+                    </div>
+                    <img onClick={() => playSound()} src={speaker} alt="speaker"/>
+                </div>
+                <div className="App">
+                    <div className="timer-wrapper">
+                      <CountdownCircleTimer
+                        isPlaying
+                        duration={time}
+                        colors={[["#004777", 0.33], ["#F7B801", 0.33], ["#A30000"]]}
+                        size='120'
+                        strokeWidth='7'
+                        onComplete={() => [true, 1000]}
+                      >
+                        {RenderTime}
+                      </CountdownCircleTimer>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
 }
+
+export default Sprint;
---
+++ src/pages/Sprint/timer.jsx
@@ -0,0 +1,39 @@
+import React, { useRef, useState } from "react";
+import "./style.css";
+
+function RenderTime({ remainingTime })  {
+  const currentTime = useRef(remainingTime);
+  const prevTime = useRef(null);
+  const isNewTimeFirstTick = useRef(false);
+  if (currentTime.current !== remainingTime) {
+    isNewTimeFirstTick.current = true;
+    prevTime.current = currentTime.current;
+    currentTime.current = remainingTime;
+  } else {
+    isNewTimeFirstTick.current = false;
+  }
+
+  if (remainingTime === 0) {
+    return <div className="timer">Be faster!</div>;
+  }
+
+  const isTimeUp = isNewTimeFirstTick.current;
+
+  return (
+    <div className="time-wrapper">
+      <div key={remainingTime} className={`time ${isTimeUp ? "up" : ""}`}>
+        {remainingTime}
+      </div>
+      {prevTime.current !== null && (
+        <div
+          key={prevTime.current}
+          className={`time ${!isTimeUp ? "down" : ""}`}
+        >
+          {prevTime.current}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default RenderTime
