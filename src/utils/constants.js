8975e369f6e313e368db5621886479b4b9715f78
Andrey Lavrov <fanton96mailru@gmail.com>
2020-07-02 13:16:36 +0300

    fix: delete comment

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,2 @@
 export const API = 'https://afternoon-falls-25894.herokuapp.com/';
 export const URI = 'https://raw.githubusercontent.com/tastypurgen/rslang-data/master/';
-
-// export { URI, API };
