2f3056aa505bcbacff150dbedfd2b010d60f5785
NikitaKolpinskiy <nikitakolpinski@mail.ru>
2020-07-11 16:20:45 +0300

    Add: finish html and css, route

--- src/pages/Promo/Promo.jsx
+++ src/pages/Promo/Promo.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import './Promo.scss';
 
@@ -10,6 +11,16 @@ import study from './images/stydied.png';
 import book from './images/book.png';
 import idea from './images/idea.png';
 import country from './images/country.png';
+import checked from './images/checked.png';
+import firstBullet from './images/firstBullet.png';
+import secondBullet from './images/secondBullet.png';
+import thirdBullet from './images/thirdBullet.png';
+import fourthBullet from './images/fourthBullet.png';
+import fifthBullet from './images/fifthBullet.png';
+import tasks from './images/tasks.png';
+import lp from './images/lp.png';
+import games from './images/games.png';
+import dictionary from './images/words.png'
 
 const Promo = () => (
   <div className="promo-section">
@@ -85,6 +96,160 @@ const Promo = () => (
         <iframe src="http://www.youtube.com/embed/W7qWa52k-nE"
         width="460" height="285" frameborder="0" allowfullscreen></iframe>
       </div>
+    </section>
+    <section className="advantages-section">
+      <div className="advantage-wrapper">
+        <h3>Преимущества RS Lang</h3>
+        <ul className="advatnage-list">
+          <li>
+            <div>
+              <img src={checked} alt="checked" />
+              <span>
+                Изучай до 6000 самых распространенных слов.
+              </span>
+            </div>
+            <div>
+              <img src={checked} alt="checked" />
+              <span>
+                Гибкие настройки обучения.
+              </span>
+            </div>
+            <div>
+              <img src={checked} alt="checked" />
+              <span>
+                Учи слова в мини-играх.
+              </span>
+            </div>
+          </li>
+          <li>
+            <div>
+              <img src={checked} alt="checked" />
+              <span>
+                С помощью списка слов можно легко отслеживать 
+                свои результаты обучения.
+              </span>
+            </div>
+            <div>
+              <img src={checked} alt="checked" />
+              <span>
+                ue accusantium omnis facere facilis quaerat. El
+              </span>
+            </div>
+            <div>
+              <img src={checked} alt="checked" />
+              <span>
+                Lorem ipsum, dolor sit amet consectetur 
+              </span>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+    <section className="method-description">
+      <div className="method-description_wrapper">
+        <div className="description-article">
+          <h3>
+            Уровни изучения RS Lang
+          </h3>
+          <p>
+            В алгоритме RS Lang применяется методика интервального повторения, 
+            в рамках котрой слова для упражнений отображаются чаще или реже. бла бла бла
+            Упражняйтесь как можно чаще, и всегда прилагайте максимум усилий , чтобы ответить правильно.
+          </p>
+        </div>
+        <div className="bullets-section">
+          <div className="bullets-container">
+            <div>
+              <img src={fifthBullet} alt="bullet"/>
+              <span>
+                У вас прекрасная память!
+              </span>
+            </div>
+            <div>
+              <img src={fourthBullet} alt="bullet"/>
+              <span>
+                Это слово так и вертится у вас на языке!
+              </span>
+            </div>
+            <div>
+              <img src={thirdBullet} alt="bullet"/>
+              <span>
+                Вы в процессе запоминания этого слова.
+              </span>
+            </div>
+            <div>
+              <img src={secondBullet} alt="bullet"/>
+              <span>
+                Это слово надо подучить.
+              </span>
+            </div>
+            <div>
+              <img src={firstBullet} alt="bullet"/>
+              <span>
+                Новое слово! Вам оно ещё не встречалось.
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section className="products-section">
+      <div className="products-title">
+        <h3>Продукты</h3>
+        <span>Выберите чем хотите заняться:</span>
+      </div>
+      <ul className="products-list">
+        <li>
+          <NavLink className="product-nav" to="">
+            <img src={tasks} alt="" />
+            <div>
+              <h3>
+                Карточки
+              </h3>
+              <span>
+                Развивай словарный запас изучая
+                карточки со словами
+              </span>
+            </div>
+          </NavLink>
+          <NavLink className="product-nav" to="/settings">
+            <img src={lp} alt="" />
+            <div>
+              <h3>
+                Личный план
+              </h3>
+              <span>
+                Поставь перед собой цели и достигай их!
+              </span>
+            </div>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className="product-nav" to="/games-panel">
+            <img src={games} alt="" />
+            <div>
+              <h3>
+                Игры
+              </h3>
+              <span>
+                Мини-игры для совершенствования
+                английского
+              </span>
+            </div>
+          </NavLink>
+          <NavLink className="product-nav" to="/dictionary">
+            <img src={dictionary} alt="" />
+            <div>
+              <h3>
+                Словарь
+              </h3>
+              <span>
+                Повторяй сложные слова
+              </span>
+            </div>
+          </NavLink>
+        </li>
+      </ul>
     </section>
   </div>
 );
