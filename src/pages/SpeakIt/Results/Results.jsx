704e46086818fa9ad94703f0a463f937c11e1a17
Pan Artur <artur.pankov.16@list.ru>
2020-07-11 04:03:53 +0300

    feat: add adaptive styles

--- src/pages/SpeakIt/Results/Results.jsx
+++ src/pages/SpeakIt/Results/Results.jsx
@@ -1,26 +1,11 @@
-import React, { useState } from 'react';
-import './Results.css';
+import React from 'react';
+import './Results.scss';
 import playAudioFunction from '../utils/playAudioFunction';
-import getTheWordTranslation from '../services/getTheWordTranslation';
 
 const Results = (props) => {
-  const [translatedWords, setTranslatedWords] = useState([]);
-  const [translateIsLoaded, setTranslateIsLoaded] = useState(false);
   const {
     rightWordsObject, data, toShowResults, toStartNewGame,
   } = props;
-
-  if (!translateIsLoaded) {
-    const wordsToTranslate = [];
-    for (let i = 0; i < data.length; i++) {
-      wordsToTranslate.push(data[i].word);
-    }
-
-    getTheWordTranslation(wordsToTranslate.join('|')).then((response) => {
-      setTranslatedWords(response.text[0].split('|'));
-      setTranslateIsLoaded(true);
-    });
-  }
 
   return (
     <div className="Results__container">
@@ -33,7 +18,8 @@ const Results = (props) => {
 
           return (
             <li
-              key={i}
+              role="button"
+              key={i.toString()}
               onClick={() => {
                 if (window.isClickEnabled) {
                   playAudioFunction(`https://raw.githubusercontent.com/Koptohhka/rslang-data/master/${it.audio}`);
@@ -41,27 +27,43 @@ const Results = (props) => {
               }}
               className={classes.join(' ')}
             >
-              {it.word}
-              {' '}
-              {it.transcription}
-              {' '}
-              {translateIsLoaded ? translatedWords[i] : ''}
+              <div className="Results__icon">
+                <svg fill="black" className="svg-inline--fa fa-volume-up fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M215.03 71.05L126.06 160H24c-13.26 0-24 10.74-24 24v144c0 13.25 10.74 24 24 24h102.06l88.97 88.95c15.03 15.03 40.97 4.47 40.97-16.97V88.02c0-21.46-25.96-31.98-40.97-16.97zm233.32-51.08c-11.17-7.33-26.18-4.24-33.51 6.95-7.34 11.17-4.22 26.18 6.95 33.51 66.27 43.49 105.82 116.6 105.82 195.58 0 78.98-39.55 152.09-105.82 195.58-11.17 7.32-14.29 22.34-6.95 33.5 7.04 10.71 21.93 14.56 33.51 6.95C528.27 439.58 576 351.33 576 256S528.27 72.43 448.35 19.97zM480 256c0-63.53-32.06-121.94-85.77-156.24-11.19-7.14-26.03-3.82-33.12 7.46s-3.78 26.21 7.41 33.36C408.27 165.97 432 209.11 432 256s-23.73 90.03-63.48 115.42c-11.19 7.14-14.5 22.07-7.41 33.36 6.51 10.36 21.12 15.14 33.12 7.46C447.94 377.94 480 319.54 480 256zm-141.77-76.87c-11.58-6.33-26.19-2.16-32.61 9.45-6.39 11.61-2.16 26.2 9.45 32.61C327.98 228.28 336 241.63 336 256c0 14.38-8.02 27.72-20.92 34.81-11.61 6.41-15.84 21-9.45 32.61 6.43 11.66 21.05 15.8 32.61 9.45 28.23-15.55 45.77-45 45.77-76.88s-17.54-61.32-45.78-76.86z" /></svg>
+              </div>
+              <span>
+                {it.word}
+                {' '}
+                {it.transcription}
+                {' '}
+                {it.wordTranslate}
+              </span>
             </li>
           );
         })}
       </ul>
-      <button onClick={() => {
-        toShowResults();
-      }}
-      >
-        return
-      </button>
-      <button onClick={() => {
-        toStartNewGame();
-      }}
-      >
-        new game
-      </button>
+      <p className="Results__statistics-description">
+        {`Произнесено ${Object.keys(rightWordsObject).length} слов из 10`}
+      </p>
+      <div className="Result__button-container">
+        <button
+          className="Result__return"
+          type="button"
+          onClick={() => {
+            toShowResults();
+          }}
+        >
+          return
+        </button>
+        <button
+          className="Result__new-game"
+          type="button"
+          onClick={() => {
+            toStartNewGame();
+          }}
+        >
+          new game
+        </button>
+      </div>
     </div>
   );
 };
--- src/pages/SpeakIt/image/Image.jsx
+++ src/pages/SpeakIt/image/Image.jsx
@@ -1,22 +1,29 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Image.scss';
 import SpeechInput from './SpeechInput/SpeechInput';
 
 const Image = (props) => {
-    const { isSpeechRecognitionOn, imageCaption, changeWordActiveElement, ChangeRightWordsObject, data, imagePath } = props;
-    return (
-        <div className="Image">
-            <img className="Image__image" src={imagePath} alt="" />
-            {
-                isSpeechRecognitionOn ?
+  const {
+    isSpeechRecognitionOn,
+    imageCaption,
+    changeWordActiveElement, ChangeRightWordsObject, data, imagePath,
+  } = props;
+  return (
+    <div className="Image">
+      <img className="Image__image" src={imagePath} alt="" />
+      {
+                isSpeechRecognitionOn
+                  ? (
                     <SpeechInput
-                        data={data}
-                        changeWordActiveElement={changeWordActiveElement}
-                        ChangeRightWordsObject={ChangeRightWordsObject} /> :
-                    <p className="Image__caption">{imageCaption}</p>
+                      data={data}
+                      changeWordActiveElement={changeWordActiveElement}
+                      ChangeRightWordsObject={ChangeRightWordsObject}
+                    />
+                  )
+                  : <p className="Image__caption">{imageCaption}</p>
             }
-        </div>
-    )
-}
+    </div>
+  );
+};
 
 export default Image;
